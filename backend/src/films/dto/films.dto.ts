efae90fd0c9ac3d0fcf7afc27b243db9f45ecd59
Ilya Lonshakov <modnyi-l@mail.ru>
2026-08-22 16:07:09 +0700

    feat: validate request dto and return 200 for orders

--- backend/src/films/dto/films.dto.ts
+++ backend/src/films/dto/films.dto.ts
@@ -1,35 +1,104 @@
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUUID,
+  Min,
+  ValidateNested,
+} from 'class-validator';
+
 export class FilmDto {
+  @IsUUID()
   id: string;
+
+  @IsNumber()
   rating: number;
+
+  @IsString()
+  @IsNotEmpty()
   director: string;
+
+  @IsArray()
+  @IsString({ each: true })
   tags: string[];
+
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
   about: string;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
+
+  @IsString()
+  @IsNotEmpty()
   image: string;
+
+  @IsString()
+  @IsNotEmpty()
   cover: string;
 }
 
 export class ScheduleDto {
+  @IsUUID()
   id: string;
+
+  @IsString()
+  @IsNotEmpty()
   daytime: string;
+
+  @IsInt()
   hall: number;
+
+  @IsInt()
+  @Min(1)
   rows: number;
+
+  @IsInt()
+  @Min(1)
   seats: number;
+
+  @IsNumber()
+  @Min(0)
   price: number;
+
+  @IsArray()
+  @IsString({ each: true })
   taken: string[];
 }
 
 export class FilmDetailsDto extends FilmDto {
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ScheduleDto)
   schedule: ScheduleDto[];
 }
 
 export class FilmsResponseDto {
+  @IsInt()
+  @Min(0)
   total: number;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => FilmDto)
   items: FilmDto[];
 }
 
 export class ScheduleResponseDto {
+  @IsInt()
+  @Min(0)
   total: number;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ScheduleDto)
   items: ScheduleDto[];
 }
--- backend/src/order/dto/order.dto.ts
+++ backend/src/order/dto/order.dto.ts
@@ -1,23 +1,71 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsEmail,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUUID,
+  Min,
+  ValidateNested,
+} from 'class-validator';
+
 export class TicketDto {
+  @IsUUID()
   film: string;
+
+  @IsUUID()
   session: string;
+
+  @IsString()
+  @IsNotEmpty()
   daytime: string;
+
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
   row: number;
+
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
   seat: number;
+
+  @Type(() => Number)
+  @IsNumber()
+  @Min(0)
   price: number;
 }
 
 export class CreateOrderDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   phone: string;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => TicketDto)
   tickets: TicketDto[];
 }
 
 export class OrderedTicketDto extends TicketDto {
+  @IsUUID()
   id: string;
 }
 
 export class OrderResponseDto {
+  @IsInt()
+  @Min(0)
   total: number;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => OrderedTicketDto)
   items: OrderedTicketDto[];
 }
